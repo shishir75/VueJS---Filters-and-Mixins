e041233d74858f2839ef7ec19f534badf4725155
shishir75 <shishir.srdr16@gmail.com>
2020-03-07 00:48:00 +0600

    Creating a Local Filter

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,32 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="container my-5">
+        <div class="row">
+            <div class="col-xs-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3">
+                <h1>Filters & Mixins</h1>
+                <p>{{ text | toUppercase }}</p>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default {
+      name: 'App',
+      data() {
+          return {
+              text: 'Hello there'
+          }
+      },
+      filters: {
+          toUppercase(value) {
+              return value.toUpperCase();
+          }
+      }
+
   }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
